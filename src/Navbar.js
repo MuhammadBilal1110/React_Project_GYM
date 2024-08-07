df47df4c8c7fc826e3df32e874bcf8db480dcfed
Muhammad <bilaljaved.iiui@gmail.com>
2024-08-07 09:52:49 +0500

    Updated styles and components for AboutUs, Navbar, and Signup pages

--- src/Navbar.js
+++ src/Navbar.js
@@ -70,6 +70,44 @@
 // }
 
 // export default Navbar;
+// import React from 'react';
+// import './Navbar.css';
+// import { Link } from 'react-router-dom';
+// import logo from './Logo.jpg'; // Ensure this matches the file name
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//       <div className="container-fluid">
+//         <Link className="navbar-brand" to="#">
+//           <img src={logo} alt="Logo" className="navbar-logo" />
+//         </Link>
+//         <span className="navbar-text">Welcome To Our GYM Center. "Unlock Your Potential"</span>
+//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon" />
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//           <ul className="navbar-nav ms-auto">
+//             <li className="nav-item">
+//               <Link className="nav-link active" aria-current="page" to="/Home">Home</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/AboutUs">AboutUs</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/FitnessCalculator">FitnessCalculator</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/ContactUs">ContactUs</Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
 import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
@@ -82,7 +120,7 @@ const Navbar = () => {
         <Link className="navbar-brand" to="#">
           <img src={logo} alt="Logo" className="navbar-logo" />
         </Link>
-        <span className="navbar-text">Welcome To Our GYM Center. "Unlock Your Potential"</span>
+        <span className="navbar-text d-none d-lg-block">Welcome To Our GYM Center. "Unlock Your Potential"</span>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
